cf8ef8fab6c163516c4048942a96892c8a390adb
Priyankateke <priiyankateke1705@gmail.com>
2020-05-29 22:59:46 +0530

    ADDED: Give Differnt Name To Default Component Greet

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { render } from 'react-dom';
-import Greet from './components/Greet'
+
+//differnt component name of Greet
+import MyComponent from './components/Greet'
 
 class App extends Component{
   render() {
     return (
       <div className="App">
-        <Greet/>
+        <MyComponent />
       </div>
     );
   }
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -3,4 +3,5 @@ import React from 'react'
 //Arrow Function
 const Greet=() => <h1>Hello</h1>
 
+//default component : can be use any name in app.js
 export default Greet;
