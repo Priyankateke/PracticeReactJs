eefc1101e9092436f5df687bd63718cb10cc8664
Priyankateke <priiyankateke1705@gmail.com>
2020-05-29 23:05:34 +0530

    REFACTORED: Export Before Arrow Function

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import { render } from 'react-dom';
 
-//differnt component name of Greet
-import MyComponent from './components/Greet'
+//Give the same name to component if we export component in same line
+import { Greet } from './components/Greet'
 
 class App extends Component{
   render() {
     return (
       <div className="App">
-        <MyComponent />
+        <Greet />
       </div>
     );
   }
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 //Arrow Function
-const Greet=() => <h1>Hello</h1>
+export const Greet=() => <h1>Hello</h1>
 
 //default component : can be use any name in app.js
-export default Greet;
