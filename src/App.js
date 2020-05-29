2d29902fc0f0e009c6057ce35e657cd2f22edbe5
Priyankateke <priiyankateke1705@gmail.com>
2020-05-30 00:16:05 +0530

    ADDED: JSX Version Of Component Hello

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ import { render } from 'react-dom';
 //Give the same name to component if we export component in same line
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
+import Hello from './components/Hello'
 
 class App extends Component{
   render() {
     return (
       <div className="App">
-        <Greet />
-        <Welcome />
+        {/* <Greet /> */}
+        {/* <Welcome /> */}
+        <Hello />
       </div>
     );
   }
