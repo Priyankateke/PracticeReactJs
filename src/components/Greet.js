261a7bc0b3fe749ee43e33bfb1d10d1e8243aaec
Priyankateke <priiyankateke1705@gmail.com>
2020-05-30 10:50:31 +0530

    REFACTORED: Reuse Component Greet And Display Name

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 //Arrow Function
-const Greet=() => <h1>Hello</h1>
+const Greet=() => <h1>Hello Priyanka</h1>
 
 //default component : can be use any name in app.js
 export default Greet
