63a292afc4b4b4a55029ae31821952627acf3956
Priyankateke <priiyankateke1705@gmail.com>
2020-05-30 10:27:34 +0530

    ADDED: React.createElement

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 const Hello = () => {
-    return(
-        <div>
-            <h1>Hello</h1>
-        </div>
-    )
+    // return(
+    //     <div>
+    //         <h1>Hello</h1>
+    //     </div>
+    // )
+    return React.createElement('div',null,'Hello Priyanka')
 }
 
 export default Hello
